7ea7283a22cf1d86c8ecfd165c174d4eb8726d5c
Pierre CLEMENT <pierre@clement.re>
2016-02-09 15:01:15 +0100

    feat: récupération des infos des dojos

---
+++ build.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var rp = require('request-promise');
+var S = require('string');
+
+var config = require('./config.js');
+
+class GithubApiConfigRequete {
+  constructor(uri) {
+    this.uri = uri;
+    this.json = true;
+    this.qs = { access_token: '' },
+    this.headers = { 'User-Agent': 'Travis build' }
+  }
+}
+rp(new GithubApiConfigRequete('https://api.github.com/users/' + config.nomOrganisationGithub + '/repos')).then((repos) => {
+  let reposPromises = repos.map((repo) => {
+    // Repos Exclus
+    if(config.reposExclus.indexOf(repo.name) >= 0 || repo.description === '') {
+      return new Promise((resolve) => { resolve(); });
+    }
+    
+    // Repos sans meta.json
+    let metaSuccessCallback = (meta) => {
+      let nomPropre = S(repo.name.replace('dojo-', '')).humanize().s;
+      let repoUtilise = {
+        nom: repo.name,
+        nomPropre: nomPropre,
+        description: repo.description,
+        url: repo.html_url,
+        meta: meta
+      };
+      return new Promise((resolve) => { resolve(repoUtilise); });
+    };
+    let metaRejectCallback = () => {
+      return new Promise((resolve) => { resolve(); });
+    };
+    return rp(new GithubApiConfigRequete('https://github.com/' + config.nomOrganisationGithub + '/'+repo.name+'/raw/master/meta.json')).then(metaSuccessCallback, metaRejectCallback);
+  });
+  
+  
+  Promise.all(reposPromises).then((reposUtilises) => {
+    let repos = reposUtilises.filter((repo) => {
+      return !!repo;
+    });
+    console.log(repos);
+  });
+});
